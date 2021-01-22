c26d31717279f862f06edb1380c8eb50d0cef211
吴永先 <wuyongxian@wondershare.cn>
2021-01-22 19:46:39 +0800

    gokit demo

---
+++ ch01/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
+	"github.com/gorilla/mux"
+)
+
+// OrderService provides operations on strings.
+type OrderService interface {
+	Uppercase(string) (string, error)
+	Count(string) int
+}
+
+// orderService is a concrete implementation of StringService
+type orderService struct{}
+
+func (orderService) Uppercase(s string) (string, error) {
+	if s == "" {
+		return "", ErrEmpty
+	}
+	return strings.ToUpper(s), nil
+}
+
+func (orderService) Count(s string) int {
+	return len(s)
+}
+
+var (
+	ErrEmpty      = errors.New("empty string")
+	ErrBadRouting = errors.New("inconsistent mapping between route and handler (programmer error)")
+)
+
+// For each method, we define request and response structs
+type uppercaseRequest struct {
+	S string `json:"s"`
+}
+
+type uppercaseResponse struct {
+	V   string `json:"v"`
+	Err string `json:"err,omitempty"` // errors don't define JSON marshaling
+}
+
+type countRequest struct {
+	S string `json:"s"`
+}
+
+type countResponse struct {
+	V int `json:"v"`
+}
+
+// Endpoints are a primary abstraction in go-kit. An endpoint represents a single RPC (method in our service interface)
+func makeUppercaseEndpoint(svc OrderService) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		req := request.(uppercaseRequest)
+		v, err := svc.Uppercase(req.S)
+		if err != nil {
+			return uppercaseResponse{v, err.Error()}, nil
+		}
+		return uppercaseResponse{v, ""}, nil
+	}
+}
+
+func makeCountEndpoint(svc OrderService) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		req := request.(countRequest)
+		v := svc.Count(req.S)
+		return countResponse{v}, nil
+	}
+}
+
+// Transports expose the service to the network. In this first example we utilize JSON over HTTP.
+func main() {
+	svc := orderService{}
+	uppercaseHandler := httptransport.NewServer(
+		makeUppercaseEndpoint(svc),
+		decodeUppercaseRequest,
+		encodeResponse,
+	)
+	countHandler := httptransport.NewServer(
+		makeCountEndpoint(svc),
+		decodeCountRequest,
+		encodeResponse,
+	)
+
+	r := mux.NewRouter()
+	r.Methods("POST").Path("/uppercase").Handler(uppercaseHandler)
+	r.Methods("GET").Path("/count/{name}").Handler(countHandler)
+	log.Fatal(http.ListenAndServe(":50050", r))
+}
+
+func decodeUppercaseRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var request uppercaseRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
+
+func decodeCountRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	vars := mux.Vars(r)
+	name, ok := vars["name"]
+	if !ok {
+		return nil, ErrBadRouting
+	}
+	var request countRequest
+	request.S = name
+	return request, nil
+}
+
+func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	return json.NewEncoder(w).Encode(response)
+}
